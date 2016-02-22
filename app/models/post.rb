e4b9955543e222b4675328c4004a41fb066276f0
mabeautiful1 <dara2xx1@gmail.com>
2016-02-22 16:01:51 +0000

    add photo slider and style login page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+    has_many :photos
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    accepts_nested_attributes_for :photos, reject_if: :all_blank, allow_destroy: true
 end

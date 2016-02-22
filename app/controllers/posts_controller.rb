04ab4c328d7ff46709c154315aff10392aa8b21c
mabeautiful1 <dara2xx1@gmail.com>
2016-02-22 14:02:44 +0000

    add photo attribute

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @image = @post.image.present?
   end
 
   # GET /posts/new
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :post_type, :description)
+      params.require(:post).permit(:title, :post_type,:image, :description)
     end
 end

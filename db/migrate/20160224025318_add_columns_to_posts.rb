3ec9cee17f887bc08d1ef603d5594a6555e32114
Narong Phath <phathnarong@gmail.com>
2016-02-25 15:51:29 +0700

    change data type

--- db/migrate/20160224025318_add_columns_to_posts.rb
+++ db/migrate/20160224025318_add_columns_to_posts.rb
@@ -1,6 +1,6 @@
 class AddColumnsToPosts < ActiveRecord::Migration
   def change
-  	add_column :posts, :price, :double
+  	add_column :posts, :price, :float
   	add_column :posts, :category, :string
   end
 end

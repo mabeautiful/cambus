ab8e9f88aeaebf2b7ebb6188701292862a53a4f8
Narong Phath <phathnarong@gmail.com>
2016-02-26 14:30:18 +0700

    Do some configuration

--- db/migrate/20160225133123_remove_columns_from_posts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromPosts < ActiveRecord::Migration
-  def change
-  	remove_column :posts, :category, :string
-  	remove_column :posts, :location, :string
-  end
-end
--- db/migrate/20160225134050_add_other_columns_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddOtherColumnsToPosts < ActiveRecord::Migration
-  def change
-  	add_column :posts, :category_id, :integer
-  	add_column :posts, :location_id, :integer
-  end
-end

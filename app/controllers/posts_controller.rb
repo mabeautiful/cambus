c4e95a5d75627f27af001f635446ea1b78b8a99d
Narong Phath <phathnarong@gmail.com>
2016-03-21 08:42:14 +0700

    abcd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,12 +19,12 @@ class PostsController < ApplicationController
     @get_amentities = @post.amentity_ids 
 
     # For development
-    if Rails.env.development?
+    if Rails.env.production?
+      @post_amentities = @get_amentities
+    else
       @amentity_to_array = @get_amentities.gsub!(/[\[\]\"]/, "")
       @amentities = @amentity_to_array.split(/,/)
       @post_amentities = @amentities
-    else
-      @post_amentities = @get_amentities
     end
 
   end

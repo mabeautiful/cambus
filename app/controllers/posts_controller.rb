61052e41d49835fb7f27b3c2b1a4f8d40fba7cc5
Narong Phath <phathnarong@gmail.com>
2016-03-21 08:01:57 +0700

    change something

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,14 @@ class PostsController < ApplicationController
     @get_amentities = @post.amentity_ids 
 
     # For development
-    @amentity_to_array = @get_amentities.gsub!(/[\[\]\"]/, "")
-    @amentities = @amentity_to_array.split(/,/)
+    if Rails.env.development?
+      @amentity_to_array = @get_amentities.gsub!(/[\[\]\"]/, "")
+      @amentities = @amentity_to_array.split(/,/)
+      @post_amentities = @amentities
+    else
+      @post_amentities = @get_amentities
+    end
+
   end
 
   # GET /posts/new
